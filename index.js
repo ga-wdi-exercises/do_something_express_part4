cf54f2c66cf5d0f2d2a1f24c41bf59b8e5b26721
RobertAKARobin <robertgfthomas@gmail.com>
2015-08-19 13:12:53 -0400

    Moar for committing to heroku

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.get("/", function(req, res){
 app.use("/", listsController);
 app.use("/", tasksController);
 
+// The process.env.PORT is for deployment to Heroku. Don't worry about it! You can have the usual:
+/*
 app.listen(3000, function(){
+*/
+app.listen(process.env.PORT || 3000, function(){
   console.log("Listening on port 3000");
 });
